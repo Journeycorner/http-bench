75b6769dc2c1e9fba0cecfa0c9dc7eada24ad724
Florian Reisecker <florian.reisecker@gmail.com>
2018-09-13 14:17:01 +0200

    Add statistics about requests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use hyper::Client;
-use hyper::rt::Future;
-use std::time::{Instant};
-use std::env;
-use tokio::runtime::Runtime;
-use hyper_tls::HttpsConnector;
 use http::Uri;
+use hyper::client::HttpConnector;
+use hyper::rt::Future;
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+use std::env;
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::time::{Duration, Instant};
+use tokio::runtime::Runtime;
 
 fn main() {
     match parse_input_arguments() {
@@ -23,7 +26,8 @@ fn parse_input_arguments() -> Option<Arguments> {
         panic!("Needs exactly two input arguments. Type usage for more information.");
     }
     // parse request repeat count
-    let number_of_requests : i32 = env::args().nth(1)
+    let number_of_requests: usize = env::args()
+        .nth(1)
         .expect("Can't access input param, that's a bug!")
         .parse()
         .expect("Input value is not a valid number!");
@@ -31,7 +35,8 @@ fn parse_input_arguments() -> Option<Arguments> {
         panic!("Request repeat count must be between 1 and 10");
     }
     // parse uri
-    let uri : Uri = env::args().nth(2)
+    let uri: Uri = env::args()
+        .nth(2)
         .expect("Can't access input param, that's a bug!")
         .parse()
         .expect("Not a valid Uri");
@@ -43,30 +48,70 @@ fn parse_input_arguments() -> Option<Arguments> {
     Some(arguments)
 }
 
-fn run(uri: Uri, number_of_requests: i32) {
+fn run(uri: Uri, number_of_requests: usize) {
     let https = HttpsConnector::new(4).expect("TLS initialization failed");
-    let client = Client::builder()
-        .build::<_, hyper::Body>(https);
-
+    let client = Client::builder().build::<_, hyper::Body>(https);
     // create a new reactor event loop
     let mut rt = Runtime::new().unwrap();
-
+    let (sender, receiver) = mpsc::channel();
     for i in 1..=number_of_requests {
-        let start = Instant::now();
-        let future = client
-            .get(uri.clone())
-            .map(move |res| {
-                println!("Response: {}", res.status());
-                println!("{}. request took {}ms to complete\n", i, start.elapsed().subsec_millis());
-            })
-            .map_err(|err| {
-                println!("Error: {}", err);
-            });
-        let _result = rt.block_on(future);
+        //let future = create_request(i, &client, uri.clone());
+        create_request(sender.clone(), &mut rt, i, &client, uri.clone());
+    }
+    let mut values: Vec<Duration> = Vec::with_capacity(number_of_requests);
+    let mut iter = receiver.try_iter();
+
+    loop {
+        let value = iter.next();
+        if value.is_none() {
+            break;
+        }
+        values.push(value.unwrap());
+    }
+    let statistic = Statistic::new(values, number_of_requests);
+    println!("{:?}", statistic);
+}
+
+fn create_request(
+    sender: Sender<Duration>,
+    runtime: &mut Runtime,
+    i: usize,
+    client: &Client<HttpsConnector<HttpConnector>>,
+    uri: Uri,
+) {
+    let start = Instant::now();
+    let future = client
+        .get(uri.clone())
+        .map(move |res| {
+            let elapsed = start.elapsed();
+            sender.send(elapsed).unwrap();
+            println!("Request took {:?}", elapsed);
+            println!("Response: {}\n", res.status());
+        }).map_err(|err| {
+            println!("Error: {}\n", err);
+        });
+    let _result = runtime.block_on(future);
+}
+
+#[derive(Debug)]
+struct Statistic {
+    average: Duration,
+    median: Duration,
+}
+
+impl Statistic {
+    fn new(values: Vec<Duration>, number_of_requests: usize) -> Statistic {
+        let sum: Duration = values.iter().sum();
+        let average = sum / number_of_requests as u32;
+        let mut values = values;
+        values.sort();
+        let middle = values.len() / 2;
+        let median = *values.iter().nth(middle).unwrap();
+        Statistic { average, median }
     }
 }
 
 struct Arguments {
-    number_of_requests: i32,
+    number_of_requests: usize,
     uri: Uri,
 }
