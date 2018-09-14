353fba5740f44fbecb7cf6a0309a470eaf8fa749
Florian Reisecker <florian.reisecker@gmail.com>
2018-09-14 17:32:22 +0200

    Modularize content

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 use http::Uri;
-use hyper::client::HttpConnector;
-use hyper::Client;
-use hyper_rustls::HttpsConnector;
 use std::env;
 use std::result::Result::{self, Err, Ok};
-use std::sync::mpsc;
-use std::sync::mpsc::Sender;
-use std::time::{Duration, Instant};
-use tokio::prelude::*;
+mod runtime;
 
 fn main() {
     match parse_input_arguments() {
         Ok(result) => {
             if let Some(arguments) = result {
-                run(&arguments.uri, arguments.number_of_requests);
+                runtime::run(&arguments.uri, arguments.number_of_requests);
             } else {
                 println!("Usage: http-bench 3 https://some-2-example-12-url.com");
             }
@@ -62,59 +56,6 @@ fn parse_input_arguments() -> Result<Option<Arguments>, String> {
         uri,
     };
     Ok(Some(arguments))
-}
-
-fn run(uri: &Uri, number_of_requests: usize) {
-    let https = HttpsConnector::new(4);
-    let client = Client::builder().build::<_, hyper::Body>(https);
-    // create a new reactor event loop
-    let (sender, receiver) = mpsc::channel();
-    for i in 1..=number_of_requests {
-        create_request(sender.clone(), i, &client, uri);
-    }
-
-    let mut telemetry: Vec<Duration> = receiver.try_iter().collect();
-    let statistic = Statistic::new(&mut telemetry, number_of_requests);
-    println!("{:?}", statistic);
-}
-
-fn create_request(
-    sender: Sender<Duration>,
-    i: usize,
-    client: &Client<HttpsConnector<HttpConnector>>,
-    uri: &Uri,
-) {
-    let start = Instant::now();
-    let future = client
-        .get(uri.clone())
-        .map(move |res| {
-            let elapsed = start.elapsed();
-            sender.send(elapsed).unwrap();
-            println!("{}. request took {:?}", i, elapsed);
-            println!("Response: {}\n", res.status());
-        }).map_err(|err| {
-            println!("Error: {}\n", err);
-        });
-
-    tokio::run(future);
-}
-
-#[derive(Debug)]
-struct Statistic {
-    average: Duration,
-    median: Duration,
-}
-
-impl Statistic {
-    fn new(values: &mut Vec<Duration>, number_of_requests: usize) -> Self {
-        values.sort();
-        let sum: Duration = values.iter().sum();
-        let average = sum / number_of_requests as u32;
-        let middle = values.len() / 2;
-        let median = values[middle];
-
-        Self { average, median }
-    }
 }
 
 struct Arguments {
---
+++ src/runtime.rs
@@ -0,0 +1,61 @@
+use http::Uri;
+use hyper::client::HttpConnector;
+use hyper::Client;
+use hyper_rustls::HttpsConnector;
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::time::{Duration, Instant};
+use tokio::prelude::Future;
+
+pub fn run(uri: &Uri, number_of_requests: usize) {
+    let https = HttpsConnector::new(4);
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    // create a new reactor event loop
+    let (sender, receiver) = mpsc::channel();
+    for i in 1..=number_of_requests {
+        create_request(sender.clone(), i, &client, uri);
+    }
+
+    let mut telemetry: Vec<Duration> = receiver.try_iter().collect();
+    let statistic = Statistic::new(&mut telemetry, number_of_requests);
+    println!("{:?}", statistic);
+}
+
+fn create_request(
+    sender: Sender<Duration>,
+    i: usize,
+    client: &Client<HttpsConnector<HttpConnector>>,
+    uri: &Uri,
+) {
+    let start = Instant::now();
+    let future = client
+        .get(uri.clone())
+        .map(move |res| {
+            let elapsed = start.elapsed();
+            sender.send(elapsed).unwrap();
+            println!("{}. request took {:?}", i, elapsed);
+            println!("Response: {}\n", res.status());
+        }).map_err(|err| {
+            println!("Error: {}\n", err);
+        });
+
+    tokio::run(future);
+}
+
+#[derive(Debug)]
+struct Statistic {
+    average: Duration,
+    median: Duration,
+}
+
+impl Statistic {
+    fn new(values: &mut Vec<Duration>, number_of_requests: usize) -> Self {
+        values.sort();
+        let sum: Duration = values.iter().sum();
+        let average = sum / number_of_requests as u32;
+        let middle = values.len() / 2;
+        let median = values[middle];
+
+        Self { average, median }
+    }
+}
